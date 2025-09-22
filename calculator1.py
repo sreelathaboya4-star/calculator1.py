d7104694b8f5d5681c677524b17e258dc867b158
sreelathaboya4-star <sreelathaboya4@gmail.com>
2025-09-22 17:21:35 +0530

    Add files via upload

---
+++ calculator1.py
@@ -0,0 +1,19 @@
+operator=input("enter an operator(+,_,*,/):")
+num1=float(input("Enter the 1st number:"))
+num2=float(input("Enter the 2nd number:"))
+
+if operator=="+":
+    result=num1+num2
+    print(round (result,3))
+elif operator=="-":
+    result=num1-num2
+    print(round(result,3))
+elif operator=="*":
+    result=num1*num2
+    print(round(result,3))
+elif operator=="/":
+    result=num1/num2
+    print(round(result,3))
+else:
+    print(f"{operator}is not a valid operator")
+
